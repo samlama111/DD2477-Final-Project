115e94b2866677ef36037ee95825579e8e184d02
Aqsa-K <aqsakausar30@gmail.com>
2024-04-17 23:37:29 +0500

    GUI, Uwer profile in ES and Book Dipslay

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, make_response
+from elasticsearch import Elasticsearch
+from werkzeug.security import generate_password_hash, check_password_hash
+from Books import *
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+
+# Setup Elasticsearch connection
+es = Elasticsearch([{'host': 'localhost', 'port': 9200, "scheme": "https"}], basic_auth=('elastic', 'l0F4vPc0pD=0kYYD-oq5'), verify_certs=False)
+
+book_manager = Book()
+
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    return redirect(url_for('register'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Search user in Elasticsearch
+        res = es.search(index="user_profiles", body={"query": {"match": {"username": username}}})
+        if res['hits']['total']['value'] > 0:
+            user = res['hits']['hits'][0]['_source']
+            if check_password_hash(user['password'], password):
+                session['logged_in'] = True
+                return redirect(url_for('search'))
+        return 'Login Failed'
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password)
+        
+        # Check if user already exists
+        res = es.search(index="user_profiles", body={"query": {"match": {"username": username}}})
+        if res['hits']['total']['value'] == 0:
+            # Add new user to Elasticsearch
+            es.index(index="user_profiles", body={"username": username, "password": hashed_password})
+            return redirect(url_for('login'))
+        return 'Username already exists'
+    return render_template('register.html')
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query', '')
+    if query:
+        books = book_manager.search_books(query)
+        res = make_response(jsonify(books), 200)
+        # for book in books:
+        #     print(book['_source']["title"] + " - " + book['_source']['description'])
+
+        return res
+    return render_template('search.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
