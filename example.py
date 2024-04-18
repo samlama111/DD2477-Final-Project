f998aa296093b2815b56fe925f6e915f87fb210e
Hildur Davíðsdóttir <54947029+hildurd99@users.noreply.github.com>
2024-04-18 22:05:42 +0200

    Loading scraped data into elastic-search

--- example.py
+++ example.py
@@ -1,41 +1,49 @@
 from elasticsearch import Elasticsearch
+import json
 
 es = Elasticsearch([{"host":"localhost", "port":9200, "scheme": "https"}], basic_auth=('elastic', 'derpoVrWlzrjV*Au7e9r'), verify_certs=False)
 
 print(es.ping())
 
-# Input data
+# Input data - uncomment the following to index
 
-# es.index(
-#  index='lord-of-the-rings',
-#  document={
-#   'character': 'Aragon',
-#   'quote': 'It is not this day.'
-#  })
+# with open('output.json', 'r') as file:
+#     data = json.load(file)
 
-# es.index(
-#  index='lord-of-the-rings',
-#  document={
-#   'character': 'Gandalf',
-#   'quote': 'A wizard is never late, nor is he early.'
-#  })
+# bookID = 0
+# for obj in data:
+#     name = obj["name"]
+#     description = obj["description"]
+#     author = obj["author"]
+#     rating = obj["rating"]
+#     num_ratings = obj["num_ratings"]
+#     num_reviews = obj["num_reviews"]
+#     genres = obj["genres"]
 
-# es.index(
-#  index='lord-of-the-rings',
-#  document={
-#   'character': 'Frodo Baggins',
-#   'quote': 'You are late'
-#  })
+#     es.index(
+#         index=bookID,
+#         document={
+#         'name': name,
+#         'description': description,
+#         'author': author,
+#         'rating': rating,
+#         'num_ratings': num_ratings,
+#         'numreviews': num_reviews,
+#         'genres': genres
+#     })
+        
+#     bookID += 1
+    
+    
+# Perform search
 
-
-
-result = es.search(index="*", query={"match": {"quote": "is"}})
+result = es.search(index="*", query={"match": {"description": "christmas"}})
 
 no_of_results = 0
+
 for hit in result['hits']['hits']:
     no_of_results += 1
     print("\n")
     print("Result", no_of_results)
-    print("Book: ", hit["_index"])
-    print("Sentence: ", hit["_source"]["quote"])
+    print("Book: ", hit["_source"]['name'])
 print("\n")
